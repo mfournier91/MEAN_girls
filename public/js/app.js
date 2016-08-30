9274cd86f820fc0753a6b57c5f566d15cfdd8cfe
General Assembly <generalassembly@Generals-MacBook-Air-7.local>
2016-08-30 19:37:19 -0400

    Angular app file is linked.

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,21 @@
 "use strict";
-
+  console.log("App file");
 (function(){
   angular
   .module("girls", [
     "ui.router",
     "ngResource"
+  ])
+  .config([
+    "$stateProvider",
+    Router
   ]);
+
+  function Router($stateProvider){
+    $stateProvider
+    .state("welcome", {
+      url: "/",
+      templateUrl: "assets/html/girls-welcome.html"
+    });
+  }
 })();
