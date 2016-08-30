ffffab4879d8799cf511916ab6f52577f2f18e56
General Assembly <generalassembly@Generals-MacBook-Air-7.local>
2016-08-30 16:12:11 -0400

    edit and delete work.

--- controllers/girlsController.js
+++ controllers/girlsController.js
@@ -21,6 +21,30 @@ var girlsController = {
     GirlModel.findById(req.params.id, function(err, doc){
       res.render("girls/show", {girl: doc})
     })
+  },
+  edit: function(req, res){
+    GirlModel.findById(req.params.id, function(err, doc){
+      res.render("girls/edit", {girl: doc})
+    })
+  },
+  update: function(req, res){
+    GirlModel.findById(req.params.id, function(err, docs){
+      docs.name = req.body.name
+      docs.offense = req.body.offense
+      docs.imageUrl = req.body.imageUrl
+      docs.save(function(err){
+        if(!err){
+          res.redirect("/girls/" + req.params.id)
+        }
+      })
+    })
+  },
+  delete: function(req, res){
+    GirlModel.remove({_id: req.params.id}, function(err){
+      if(!err){
+        res.redirect("/burnbook")
+      }
+    })
   }
 }
 
